0d8fbaca7bef3b15dfea59f316af0d78a9daa023
veeraganesh vijayaraman <veeraganesh12@gmail.com>
2025-02-10 22:24:25 +0530

    sports team rest api dev changes model added

--- src/main/java/com/sportsteam/sports/model/Player.java
+++ src/main/java/com/sportsteam/sports/model/Player.java
@@ -10,5 +10,36 @@ public class Player {
 
     Boolean isCaptain;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getCaptain() {
+        return isCaptain;
+    }
+
+    public void setCaptain(Boolean captain) {
+        isCaptain = captain;
+    }
 
 }
