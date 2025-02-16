6ab3f1432f97a673d2ce30c8253119fbd691c901
veeraganesh vijayaraman <veeraganesh12@gmail.com>
2025-02-16 23:33:18 +0530

    sports team rest api dev changes controller added

--- src/main/java/com/sportsteam/sports/controller/SportsController.java
+++ src/main/java/com/sportsteam/sports/controller/SportsController.java
@@ -1,5 +1,8 @@
 package com.sportsteam.sports.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SportsController {
 
     //sports controller
