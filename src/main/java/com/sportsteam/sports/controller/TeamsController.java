377c4933cda873e99dbf507bf1ad2eb165fb2fc3
veeraganesh vijayaraman <veeraganesh12@gmail.com>
2025-02-24 23:59:26 +0530

    sports team rest api dev changes controller updates added

--- src/main/java/com/sportsteam/sports/controller/TeamsController.java
+++ src/main/java/com/sportsteam/sports/controller/TeamsController.java
@@ -1,7 +1,12 @@
 package com.sportsteam.sports.controller;
 
+import com.sportsteam.sports.model.Team;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TeamsController {
@@ -11,6 +16,11 @@ public class TeamsController {
     @PostMapping
     public String addTeam() {
         return "team created";
+    }
+
+    @GetMapping("/getAllTeams")
+    public List<Team> getAllTeams() {
+        return new ArrayList<>();
     }
 
 }
